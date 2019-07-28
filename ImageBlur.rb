e4527cf8f639815a3843aa4ea29dbb384ea08365
vgeroim <vgeroim.qa@gmail.com>
2019-07-28 15:40:25 +0000

    initial commit

---
+++ ImageBlur.rb
@@ -0,0 +1,35 @@
+# Image Blur #1: Build Data Representation of an Image
+
+class Image
+	def initialize(matrix)
+		@matrix = matrix
+		validate_matrix(@matrix)
+	end
+
+	def is_array?(array)
+		unless array.instance_of? Array
+			puts "Please verify the data for matrix. It requires to be a two-dimentional array."
+			exit 
+		end
+	end
+
+	def validate_matrix(matrix)
+		is_array?(matrix)
+		matrix.each { |element| is_array?(element)}
+	end
+
+	def output_image
+		@matrix.each do |element|
+			puts element.join
+		end
+	end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
