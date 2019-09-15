f72c1ef121374652e769bd10b70e3c0ab7c448f3
vgeroim <vgeroim.qa@gmail.com>
2019-09-15 23:21:53 +0000

    reformat code

--- ImageBlur.rb
+++ ImageBlur.rb
@@ -1,28 +1,28 @@
 # Image Blur #1: Build Data Representation of an Image
 
 class Image
-	def initialize(matrix)
-		@matrix = matrix
-		validate_matrix(@matrix)
-	end
+  def initialize(matrix)
+    @matrix = matrix
+    validate_matrix(@matrix)
+  end
 
-	def is_array?(array)
-		unless array.instance_of? Array
-			puts "Please verify the data for matrix. It requires to be a two-dimentional array."
-			exit 
-		end
-	end
+  def is_array?(array)
+    unless array.instance_of? Array
+      puts "Please verify the data for matrix. It requires to be a two-dimentional array."
+      exit 
+    end
+  end
 
-	def validate_matrix(matrix)
-		is_array?(matrix)
-		matrix.each { |element| is_array?(element)}
-	end
+  def validate_matrix(matrix)
+    is_array?(matrix)
+    matrix.each { |element| is_array?(element)}
+  end
 
-	def output_image
-		@matrix.each do |element|
-			puts element.join
-		end
-	end
+  def output_image
+    @matrix.each do |element|
+      puts element.join
+    end
+  end
 end
 
 image = Image.new([
